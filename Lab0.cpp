68ce19f6ed718c34c3b00532d38a7ddd05595310
Shefa21 <152915092+Shefa21@users.noreply.github.com>
2024-02-20 14:52:49 +0600

    Update Lab0.cpp

--- Lab0.cpp
+++ Lab0.cpp
@@ -38,5 +38,35 @@ int main()
 
     return 0;
 }
+//task 2 - print 10+20=30 without using any int variables and the calculation should happen in the code
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"10+20= "<<10+20<<endl;
+}
+//task 3 - using switch case to check odd even
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"enter a number: "<<endl;
+    int n;
+    cin>>n;
+    switch(n%2)
+    {
+    case 0:
+        cout<<"it is an even number"<<endl;
+        break;
+    case 1:
+        cout<<"it is an odd number"<<endl;
+        break;
+    default:
+        cout<<"Invalid input"<<endl;
+        break;
+
+    }
+}
+
 
 
