f05589bab065ead521a6fbbb15145aeada44e700
Shefa21 <152915092+Shefa21@users.noreply.github.com>
2024-03-09 23:13:25 +0600

    Update Lab5.cpp

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,1 +1,13 @@
-//
+//Task 1: Recall the class we used in the previous lab to allocate memory dynamically. Modify the header file 
+and the source file given below so that they now work as template class (the array elements in the 
+dynamically allocated memory can be any type as the user defines).
+
+
+
+
+
+
+
+
+//Task 2: Recall the complex number class we discussed in our lectures. Modify the class and overload the *
+(multiplication) and the != (not equal) operators for the class given below
